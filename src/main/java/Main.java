567720de2f7c3d0ffd1208752146141a9ca2ff98
Malikeh97 <malikehehghaghi@gmail.com>
2019-02-05 14:08:45 +0330

    Register Request POJO added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
+import Models.RegisterRequest;
 import javafx.util.Pair;
+import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static boolean isFinished = false;
+    private static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         while (!isFinished) {
             Pair<String, String> commandParts = getCommandParts();
             String commandName = commandParts.getKey();
             String commandData = commandParts.getValue();
 
+
             switch (commandName) {
                 case "register":
                     System.out.println(commandData);
+                    RegisterRequest registerRequest = mapper.readValue(commandData, RegisterRequest.class);
                     break;
                 case "addProject":
                     System.out.println(commandData);
---
+++ src/main/java/Models/RegisterRequest.java
@@ -0,0 +1,16 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private List<Skill> skills;
+
+}
