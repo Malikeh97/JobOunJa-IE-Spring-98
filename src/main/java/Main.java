666e0859ca2694034814e127adc5e9829215ca0a
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-02-10 13:40:31 +0330

    Automated test with junit implemented.
    Co-authored-by: Malikeh Ehghagi <malikehehghaghi@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+
+
 import Models.*;
 import javafx.util.Pair;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -7,180 +9,179 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-    private static boolean isFinished = false;
-    private static ObjectMapper mapper = new ObjectMapper();
+	private static Scanner scanner;
+	private static boolean isFinished = false;
+	private static ObjectMapper mapper = new ObjectMapper();
 
-    private static HashMap<String, HashMap<String, Integer>> users = new HashMap<>();
-    private static List<AddProjectRequest> projects = new ArrayList<>();
-    private static List<BidRequest> bids = new ArrayList<>();
+	private static HashMap<String, HashMap<String, Integer>> users = new HashMap<>();
+	private static List<AddProjectRequest> projects = new ArrayList<>();
+	private static List<BidRequest> bids = new ArrayList<>();
 
-    public static void main(String[] args){
-        while (!isFinished) {
-            Pair<String, String> commandParts = getCommandParts();
-            String commandName = commandParts.getKey();
-            String commandData = commandParts.getValue();
+	public static void main(String[] args) {
+		setUp();
+		isFinished = false;
+		while (!isFinished) {
+			Pair<String, String> commandParts = getCommandParts();
+			String commandName = commandParts.getKey();
+			String commandData = commandParts.getValue();
 
-            try {
-                switch (commandName) {
-                    case "register":
-                        register(commandData);
-                        break;
-                    case "addProject":
-                        addProject(commandData);
-                        break;
-                    case "bid":
-                        bid(commandData);
-                        break;
-                    case "auction":
-                        auction(commandData);
-                        break;
-                }
+			try {
+				switch (commandName) {
+					case "register":
+						register(commandData);
+						break;
+					case "addProject":
+						addProject(commandData);
+						break;
+					case "bid":
+						bid(commandData);
+						break;
+					case "auction":
+						auction(commandData);
+						break;
+				}
 
-            } catch (IOException ex) {
-                System.out.println(ex.getLocalizedMessage());
-            }
+			} catch (IOException ex) {
+				System.out.println(ex.getLocalizedMessage());
+			}
+		}
+	}
 
-        }
-    }
+	private static void setUp() {
+		users = new HashMap<>();
+		projects = new ArrayList<>();
+		bids = new ArrayList<>();
+		scanner = new Scanner(System.in);
+	}
 
-    private static void register(String commandData) throws IOException {
-        System.out.println(commandData);
-        RegisterRequest registerRequest = mapper.readValue(commandData, RegisterRequest.class);
-        if (users.containsKey(registerRequest.getUsername())) {
-            System.out.println("ERROR: " + registerRequest.getUsername() + ", This username exists.");
-        } else {
-            HashMap<String, Integer> skills = new HashMap<>();
-            for(Skill skill : registerRequest.getSkills()) {
-                skills.put(skill.getName(), skill.getPoints());
-            }
-            users.put(registerRequest.getUsername(), skills);
-        }
-    }
+	private static void register(String commandData) throws IOException {
+		RegisterRequest registerRequest = mapper.readValue(commandData, RegisterRequest.class);
+		if (users.containsKey(registerRequest.getUsername())) {
+			System.err.println("ERROR: " + registerRequest.getUsername() + ", This username exists.");
+		} else {
+			HashMap<String, Integer> skills = new HashMap<>();
+			for (Skill skill : registerRequest.getSkills()) {
+				skills.put(skill.getName(), skill.getPoints());
+			}
+			users.put(registerRequest.getUsername(), skills);
+		}
+	}
 
-    private static void addProject(String commandData) throws IOException {
-        System.out.println(commandData);
-        AddProjectRequest addProjectRequest = mapper.readValue(commandData, AddProjectRequest.class);
-        if(getProject(addProjectRequest.getTitle()) == null) {
-            System.out.println("ERROR: " + "This project title exists.");
-        } else {
-            projects.add(addProjectRequest);
-        }
+	private static void addProject(String commandData) throws IOException {
+		AddProjectRequest addProjectRequest = mapper.readValue(commandData, AddProjectRequest.class);
+		if (getProject(addProjectRequest.getTitle()) == null) {
+			projects.add(addProjectRequest);
+		} else {
+			System.err.println("ERROR: " + "This project title exists.");
+		}
+	}
 
-    }
+	private static void bid(String commandData) throws IOException {
+		BidRequest bidRequest = mapper.readValue(commandData, BidRequest.class);
+		if (isEligibleBid(bidRequest)) {
+			BidRequest bid = getBid(bidRequest.getProjectTitle(), bidRequest.getBiddingUser());
+			if (bid == null)
+				bids.add(bidRequest);
+			else
+				bid.setBidAmount(bidRequest.getBidAmount());
+		} else {
+			System.err.println("ERROR: " + "Bid is not eligible.");
+		}
+	}
 
-    private static void bid(String commandData) throws IOException {
-        System.out.println(commandData);
-        BidRequest bidRequest = mapper.readValue(commandData, BidRequest.class);
-        if(isEligibleBid(bidRequest)) {
-            BidRequest bid = getBid(bidRequest.getProjectTitle(), bidRequest.getBiddingUser());
-            if (bid == null)
-                bids.add(bidRequest);
-            else
-                bid.setBidAmount(bidRequest.getBidAmount());
-        } else {
-            System.out.println("ERROR: " + "Bid is not eligible.");
-        }
-    }
+	private static void auction(String commandData) throws IOException {
+		AuctionRequest auctionRequest = mapper.readValue(commandData, AuctionRequest.class);
+		AddProjectRequest project = getProject(auctionRequest.getProjectTitle());
+		if (project == null) {
+			System.out.println("No such project found.");
+		} else {
+			List<BidRequest> projectBids = getAllProjectBids(auctionRequest.getProjectTitle());
+			List<String> winners = getWinners(project, projectBids);
+			if (winners == null || winners.size() == 0) {
+				System.out.println("No bid for this project exists.");
+			} else if (winners.size() == 1) {
+				System.out.println("Winner is: " + winners.get(0));
+			} else {
+				System.out.println("Winners are: " + winners.toString());
+			}
+		}
 
-    private static void auction(String commandData) throws IOException {
-        System.out.println(commandData);
-        AuctionRequest auctionRequest = mapper.readValue(commandData, AuctionRequest.class);
-        AddProjectRequest project = getProject(auctionRequest.getProjectTitle());
-        if (project == null) {
-            System.out.println("No such project found.");
-        } else {
-            List<BidRequest> projectBids = getAllProjectBids(auctionRequest.getProjectTitle());
-            List<String> winners = getWinners(project, projectBids);
-            if (winners == null || winners.size() == 0) {
-                System.out.println("No bid for this project exists.");
-            } else if (winners.size() == 1) {
-                System.out.println("Winner is: " + winners.get(0));
-            } else {
-                System.out.println("Winners are: " + winners.toString());
-            }
-        }
+		isFinished = true;
+	}
 
-        isFinished = true;
-    }
+	private static boolean isEligibleBid(BidRequest bidRequest) {
+		AddProjectRequest project = getProject(bidRequest.getProjectTitle());
+		HashMap<String, Integer> userSkills = users.get(bidRequest.getBiddingUser());
+		if (project == null || userSkills == null)
+			return false;
 
-    private static boolean isEligibleBid(BidRequest bidRequest) {
-        AddProjectRequest project = getProject(bidRequest.getProjectTitle());
-        HashMap<String, Integer> userSkills = users.get(bidRequest.getBiddingUser());
-        if(project == null || userSkills == null)
-            return false;
+		if (bidRequest.getBidAmount() > project.getBudget())
+			return false;
 
-        if(bidRequest.getBidAmount() > project.getBudget())
-            return false;
+		for (Skill skill : project.getSkills()) {
+			if (!userSkills.containsKey(skill.getName()))
+				return false;
+			if (skill.getPoints() > userSkills.get(skill.getName()))
+				return false;
+		}
+		return true;
+	}
 
-        for (Skill skill : project.getSkills()) {
-            if (!userSkills.containsKey(skill.getName()))
-                return false;
-            if(skill.getPoints() > userSkills.get(skill.getName()))
-                return false;
-        }
-        return true;
-    }
+	private static AddProjectRequest getProject(String projectTitle) {
+		return projects.stream()
+				.filter(project -> project.getTitle().equals(projectTitle))
+				.findFirst()
+				.orElse(null);
+	}
 
-    private static AddProjectRequest getProject(String projectTitle) {
-        return projects.stream()
-                .filter(project -> project.getTitle().equals(projectTitle))
-                .findFirst()
-                .orElse(null);
-    }
+	private static List<BidRequest> getAllProjectBids(String projectTitle) {
+		return bids.stream()
+				.filter(bid -> bid.getProjectTitle().equals(projectTitle))
+				.collect(Collectors.toList());
+	}
 
-    private static List<BidRequest> getAllProjectBids(String projectTitle) {
-        return bids.stream()
-                .filter(bid -> bid.getProjectTitle().equals(projectTitle))
-                .collect(Collectors.toList());
-    }
+	private static BidRequest getBid(String projectTitle, String biddingUser) {
+		return bids.stream()
+				.filter(bid -> bid.getProjectTitle().equals(projectTitle) && bid.getBiddingUser().equals(biddingUser))
+				.findFirst()
+				.orElse(null);
+	}
 
-    private static BidRequest getBid(String projectTitle, String biddingUser) {
-        return bids.stream()
-                .filter(bid -> bid.getProjectTitle().equals(projectTitle) && bid.getBiddingUser().equals(biddingUser))
-                .findFirst()
-                .orElse(null);
-    }
+	private static List<String> getWinners(AddProjectRequest project, List<BidRequest> projectBids) {
+		if (projectBids == null || projectBids.size() == 0) return new ArrayList<>();
 
-    private static List<String> getWinners(AddProjectRequest project, List<BidRequest> projectBids) {
-        if (projectBids == null || projectBids.size() == 0) return new ArrayList<>();
+		List<String> winners = new ArrayList<>();
+		double maxPoint = Double.MIN_VALUE;
+		for (BidRequest bid : projectBids) {
+			double point = getBidEvaluation(project, bid);
+			if (point > maxPoint) {
+				maxPoint = point;
+				winners.clear();
+				winners.add(bid.getBiddingUser());
+			} else if (point == maxPoint) {
+				winners.add(bid.getBiddingUser());
+			}
+		}
 
-        List<String> winners = new ArrayList<>();
-        double maxPoint = Double.MIN_VALUE;
-        for(BidRequest bid : projectBids) {
-            double point = getBidEvaluation(project, bid);
-            if(point > maxPoint) {
-                maxPoint = point;
-                winners.clear();
-                winners.add(bid.getBiddingUser());
-            }  else if(point == maxPoint) {
-                winners.add(bid.getBiddingUser());
-            }
-        }
+		return winners;
+	}
 
-        return winners;
-    }
+	private static double getBidEvaluation(AddProjectRequest project, BidRequest bid) {
+		HashMap<String, Integer> userSkills = users.get(bid.getBiddingUser());
+		double value = 0;
+		for (Skill skill : project.getSkills()) {
+			double userSkill = userSkills.get(skill.getName());
+			value += Math.pow(skill.getPoints() - userSkill, 2.0);
+		}
+		value *= 10000;
+		value += (double) (project.getBudget() - bid.getBidAmount());
 
-    private static double getBidEvaluation(AddProjectRequest project, BidRequest bid) {
-        HashMap<String, Integer> userSkills = users.get(bid.getBiddingUser());
-        double value = 0;
-        for(Skill skill : project.getSkills()) {
-            double userSkill = userSkills.get(skill.getName());
-            value += Math.pow(skill.getPoints() - userSkill, 2.0);
-        }
-        value *= 10000;
-        value += (double) (project.getBudget() - bid.getBidAmount());
+		return value;
+	}
 
-        return value;
-    }
-
-    private static Pair<String, String> getCommandParts() {
-        String command = scanner.nextLine();
-        int spaceIndex = command.indexOf(" ");
-        return new Pair<>(command.substring(0, spaceIndex), command.substring(spaceIndex));
-    }
-
-
-    // Todo: implement validation
-    // Todo: test the code
+	private static Pair<String, String> getCommandParts() {
+		String command = scanner.nextLine();
+		int spaceIndex = command.indexOf(" ");
+		return new Pair<>(command.substring(0, spaceIndex), command.substring(spaceIndex));
+	}
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,77 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.io.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class MainTest {
+
+	private ByteArrayInputStream inContent;
+	private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final InputStream systemIn = System.in;
+	private final PrintStream systemOut = System.out;
+
+	private String filePath;
+
+	@Parameters
+	public static Collection files() {
+		List<String> files = new ArrayList<>();
+		for (int i = 1; i <= 2; i++) {
+			files.add("src/test/resources/sample" + i + ".input");
+		}
+		return files;
+	}
+
+	public MainTest(String filePath) {
+		this.filePath = filePath;
+	}
+
+	@Before
+	public void setUp() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void tearDown() {
+		System.setIn(systemIn);
+		System.setOut(systemOut);
+	}
+
+	@Test
+	public void test() throws IOException {
+		Object[] testObject = readFile(filePath);
+		setInput(testObject[0].toString());
+		Main.main(new String[0]);
+
+		assertEquals("differ", testObject[1], outContent.toString());
+	}
+
+	private Object[] readFile(String path) throws FileNotFoundException {
+		StringBuilder data = new StringBuilder();
+		String expectedResult = "";
+		File file = new File(path);
+		Scanner scanner = new Scanner(file);
+
+		while (scanner.hasNextLine()) {
+			String next = scanner.nextLine();
+			if (next.equals("-"))
+				expectedResult = scanner.nextLine() + "\n";
+			else
+				data.append(next).append("\n");
+		}
+
+		return new Object[]{data, expectedResult};
+	}
+
+	private void setInput(String data) {
+		inContent = new ByteArrayInputStream(data.getBytes());
+		System.setIn(inContent);
+	}
+}
