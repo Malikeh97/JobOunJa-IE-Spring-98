431313c3e05fd31e202e1cef1bbd1cb20607c9b7
Malikeh97 <malikehehghaghi@gmail.com>
2019-02-10 10:38:27 +0330

    getWinners method implemented + handle duplicate bid from a user for a project
    Co-authored-by: Ali Tabatabaei <a.tabatabaei97@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
@@ -72,10 +73,35 @@ public class Main {
         System.out.println(commandData);
         BidRequest bidRequest = mapper.readValue(commandData, BidRequest.class);
         if(isEligibleBid(bidRequest)) {
-            bids.add(bidRequest);
+            BidRequest bid = getBid(bidRequest.getProjectTitle(), bidRequest.getBiddingUser());
+            if (bid == null)
+                bids.add(bidRequest);
+            else
+                bid.setBidAmount(bidRequest.getBidAmount());
         } else {
             System.out.println("ERROR: " + "Bid is not eligible.");
         }
+    }
+
+    private static void auction(String commandData) throws IOException {
+        System.out.println(commandData);
+        AuctionRequest auctionRequest = mapper.readValue(commandData, AuctionRequest.class);
+        AddProjectRequest project = getProject(auctionRequest.getProjectTitle());
+        if (project == null) {
+            System.out.println("No such project found.");
+        } else {
+            List<BidRequest> projectBids = getAllProjectBids(auctionRequest.getProjectTitle());
+            List<String> winners = getWinners(project, projectBids);
+            if (winners == null || winners.size() == 0) {
+                System.out.println("No bid for this project exists.");
+            } else if (winners.size() == 1) {
+                System.out.println("Winner is: " + winners.get(0));
+            } else {
+                System.out.println("Winners are: " + winners.toString());
+            }
+        }
+
+        isFinished = true;
     }
 
     private static boolean isEligibleBid(BidRequest bidRequest) {
@@ -98,16 +124,54 @@ public class Main {
 
     private static AddProjectRequest getProject(String projectTitle) {
         return projects.stream()
-                .filter(p -> p.getTitle().equals(projectTitle))
+                .filter(project -> project.getTitle().equals(projectTitle))
                 .findFirst()
                 .orElse(null);
     }
 
-    private static void auction(String commandData) throws IOException {
-        System.out.println(commandData);
-        AuctionRequest auctionRequest = mapper.readValue(commandData, AuctionRequest.class);
-        // Todo: implement functionality
-        isFinished = true;
+    private static List<BidRequest> getAllProjectBids(String projectTitle) {
+        return bids.stream()
+                .filter(bid -> bid.getProjectTitle().equals(projectTitle))
+                .collect(Collectors.toList());
+    }
+
+    private static BidRequest getBid(String projectTitle, String biddingUser) {
+        return bids.stream()
+                .filter(bid -> bid.getProjectTitle().equals(projectTitle) && bid.getBiddingUser().equals(biddingUser))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static List<String> getWinners(AddProjectRequest project, List<BidRequest> projectBids) {
+        if (projectBids == null || projectBids.size() == 0) return new ArrayList<>();
+
+        List<String> winners = new ArrayList<>();
+        double maxPoint = Double.MIN_VALUE;
+        for(BidRequest bid : projectBids) {
+            double point = getBidEvaluation(project, bid);
+            if(point > maxPoint) {
+                maxPoint = point;
+                winners.clear();
+                winners.add(bid.getBiddingUser());
+            }  else if(point == maxPoint) {
+                winners.add(bid.getBiddingUser());
+            }
+        }
+
+        return winners;
+    }
+
+    private static double getBidEvaluation(AddProjectRequest project, BidRequest bid) {
+        HashMap<String, Integer> userSkills = users.get(bid.getBiddingUser());
+        double value = 0;
+        for(Skill skill : project.getSkills()) {
+            double userSkill = userSkills.get(skill.getName());
+            value += Math.pow(skill.getPoints() - userSkill, 2.0);
+        }
+        value *= 10000;
+        value += (double) (project.getBudget() - bid.getBidAmount());
+
+        return value;
     }
 
     private static Pair<String, String> getCommandParts() {
@@ -116,6 +180,7 @@ public class Main {
         return new Pair<>(command.substring(0, spaceIndex), command.substring(spaceIndex));
     }
 
+
     // Todo: implement validation
-    // Todo:test the code
+    // Todo: test the code
 }
