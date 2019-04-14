9590b7637516f6d064220ecc80d1e23746c62aa1
Malikeh97 <malikehehghaghi@gmail.com>
2019-04-14 12:42:17 +0430

    project page html moved to react form
    Co-authored-by: Ali Tabatabaei <a.tabatabaei97@gmail.com>

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 
 import NavBar from "./components/common/navbar";
 import Profile from "./components/profile";
+import Project from "./components/project/project";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
@@ -19,6 +20,7 @@ class App extends Component {
                         <div className="page-details">
                             <Switch>
                                 <Route path="/profile/:id" component={Profile}/>
+                                <Route path="/projects/:id" component={Project}/>
                             </Switch>
                         </div>
                     </main>
