1a94f64068efc9560f95fbabe11ad0343adba82d
Malikeh97 <malikehehghaghi@gmail.com>
2019-04-14 23:48:31 +0430

    project-info component implemented/ dynamic image addressing left
    Co-authored-by: Ali Tabatabaei <a.tabatabaei97@gmail.com>

---
+++ react-app/src/components/common/project_info.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './project_info.css';
+
+const ProjectInfo = ({ infoId, altIcon, iconSrc, featureText, valueText, timeIsUp }) => {
+    let featureClass = "static"
+    let valueClass = "text"b
+
+    if (timeIsUp) {
+        featureClass = featureClass + ' red'
+    } else if (infoId === 'budget') {
+        featureClass = featureClass + ' job-ounja-primary-color-text'
+        valueClass = featureClass + ' job-ounja-primary-color-text'
+    }
+    return (
+        <div id={infoId}>
+            <img src={require( "../../assets/" + (infoId === 'budget'? "money-bag.svg" : (infoId === 'winner'? "check-mark.svg" : "deadline.svg" )  ))} alt={altIcon}/>
+            <span className={featureClass}>{featureText}</span>
+            <span className={valueClass}> {valueText} </span>
+        </div>
+    );
+};
+
+ProjectInfo.propType = {
+    infoId: PropTypes.string.isRequired,
+    altIcon: PropTypes.string.isRequired,
+    iconSrc: PropTypes.string.isRequired,
+    featureText: PropTypes.string.isRequired,
+    valueText: PropTypes.string.isRequired,
+    timeIsUp: PropTypes.bool.isRequired
+};
+
+export default ProjectInfo;
