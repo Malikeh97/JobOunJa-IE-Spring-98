ac575be9b7f9de025b896b4a607df10f361c037d
Malikeh97 <malikehehghaghi@gmail.com>
2019-04-19 22:46:45 +0430

    login page added
    Co-authored-by: Ali Tabatabaei <a.tabatabaei97@gmail.com>

---
+++ react-app/src/components/login/index.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+import './login.css';
+import {toast} from "react-toastify";
+
+
+
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="container-fluid" id="login">
+                        <div className="well card card-body">
+                            <h2 id="title">
+                                 ورود
+                            </h2>
+
+                            <form>
+                                <div className="row">
+                                    <div className="form-group col-md-6" id="username">
+                                        <label htmlFor="username-input">نام کاربری: </label>
+                                        <input type="text" className="form-control" id="username-input"/>
+                                    </div>
+                                </div>
+
+                                <div className="row">
+                                    <div className="form-group col-md-6" id="password">
+                                        <label htmlFor="password-input">رمزعبور: </label>
+                                        <input type="password" className="form-control" id="password-input"/>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <button type="submit" className="btn btn-primary" id="login-button">ورود</button>
+                                </div>
+
+                            </form>
+                        </div>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
