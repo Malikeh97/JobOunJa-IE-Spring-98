deeba973a3ff6115ab13dc7d2ed2bd17d6d63484
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-04-19 22:54:59 +0430

    sign up implemented
    Co-authored-by: Malikeh Ehghaghi <malikeh.ehghaghi@gmail.com>

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from "./components/common/navbar";
 import Profile from "./components/profile";
 import Home from "./components/home";
 import Project from "./components/project";
+import Signup from "./components/singup";
 import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends Component {
                     <NavBar/>
                     <main>
                         <Switch>
+                            <Route path="/sign-up" component={Signup}/>
                             <Route path="/profile/:id" component={Profile}/>
                             <Route path="/projects/:id" component={Project}/>
                             <Route path="/" component={Home}/>
