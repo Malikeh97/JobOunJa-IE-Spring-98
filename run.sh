b63ea69b436cf537216ff020614c37ace5a33ad0
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-05-24 16:04:01 +0430

    change structure
    Co-authored-by: Malikeh Ehghaghi <malikehehghaghi@gmail.com>

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run --rm -it -v $(pwd):/project -v ~/.m2:/root/.m2 -w /project maven mvn package && docker-compose up
+docker run --rm -it -v $(pwd):/project -v ~/.m2:/root/.m2 -w /project maven mvn package
+docker run -it -v $(pwd)/target/ali.malikeh.war:/usr/local/tomcat/webapps/ROOT.war \
+-v $(pwd)/target/ali.malikeh:/usr/local/tomcat/webapps/ROOT tomcat:9.0
